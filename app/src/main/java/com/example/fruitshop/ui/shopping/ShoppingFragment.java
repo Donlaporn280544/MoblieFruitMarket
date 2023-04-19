c2c440f9d0c1802926593860b833c5d4299a8ee0
minemint <s6304022610073@email.kmutnb.ac.th>
2023-04-19 10:27:17 +0700

    fix and update cart

--- app/src/main/java/com/example/fruitshop/ui/shopping/ShoppingFragment.java
+++ app/src/main/java/com/example/fruitshop/ui/shopping/ShoppingFragment.java
@@ -4,18 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.example.fruitshop.R;
 import com.example.fruitshop.databinding.FragmentShoppingBinding;
 
 public class ShoppingFragment extends Fragment {
 
     private FragmentShoppingBinding binding;
+
+    ImageView imgcart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,11 +29,17 @@ public class ShoppingFragment extends Fragment {
         binding = FragmentShoppingBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-<<<<<<<<< Temporary merge branch 1
 
-=========
->>>>>>>>> Temporary merge branch 2
         return root;
+    }
+
+    public ImageView Imgcart() {
+        //ใส่ภาพลงimgview
+        //imgcart   = (Image) getView().findViewById(R.id.imgcart);
+        //imgcart.setImageResource(R.drawable.bg1.jpg);
+        imgcart = (ImageView) getView().findViewById(R.id.imageView);
+        imgcart.setImageResource(R.drawable.bg1);
+        return imgcart;
     }
 
     @Override
